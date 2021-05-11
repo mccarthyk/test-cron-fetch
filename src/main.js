ddb0415fae118b20c1291ee609a9cd91d9534398
Kevin McCarthy <mccarthyk@hcflgov.net>
2021-05-11 08:59:04 -0400

    init template

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import 'bootstrap/dist/js/bootstrap.bundle'
+// import 'iframe-resizer/js/iframeResizer.contentWindow'
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: './',
+  // build: {
+  //   lib: {
+  //     entry: resolve(__dirname, 'src/main.js'),
+  //     name: 'MyLib',
+  //   },
+  // },
 })
